480c6994d8dc75c1926ccfa571a86f458b8e92ff
Julian-Hochhaus# <julian.hochhaus@tu-dortmund.de>
2022-08-11 13:55:30 +0200

    tested w tougaard background and switched back to old routine, where the background is assumed to be constant left to the spectrum

--- Python/xpspy.py
+++ Python/xpspy.py
@@ -210,7 +210,7 @@ def tougaard2(x, y, B, C, C_d, D):
         len_padded = int(50 / delta_x)
         # len_padded = 3*len(x)
         padded_x = np.concatenate((x, np.linspace(x[-1] + delta_x, x[-1] + delta_x * len_padded, len_padded)))
-        padded_y = np.concatenate((y, np.linspace(np.mean(y[-1:]),0 ,len_padded)))
+        padded_y = np.concatenate((y,np.mean(y[-1:]) * np.ones(len_padded)))
         for k in range(len(x)):
             x_k = x[k]
             bg_temp = 0
@@ -231,7 +231,7 @@ def tougaard(x, y, B, C, C_d, D):
         bgrnd[0] = y
         bg = []
         delta_x = abs((x[-1] - x[0]) / len(x))
-        len_padded = int(100 / delta_x )
+        len_padded = int(50 / delta_x )
         # len_padded = 3*len(x)
         padded_x = np.concatenate((x, np.linspace(x[-1] + delta_x, x[-1] + delta_x * len_padded, len_padded)))
         padded_y = np.concatenate((y, np.mean(y[-1:]) * np.ones(len_padded)))
