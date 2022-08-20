deef2fdb55f6d8a38b84bd77d29ba12e03541531
Hexanders <alexander.a.kononov@gmail.com>
2022-08-20 08:47:33 +0200

    Added raise_error method and catch some errors.       Note: 	Import method imp() causing problems if e.g. csv file is in wrong format. Look for the method to check the integrity of loading fieles.

--- Python/main.py
+++ Python/main.py
@@ -341,6 +341,12 @@ class PrettyWidget(QtWidgets.QMainWindow):
         self.plottitle = QtWidgets.QLineEdit()
         grid.addWidget(self.plottitle, 3, 7, 1, 2)
         self.show()
+    
+    def raise_error(self, windowTitle : str) -> None:
+        self.error_dialog.setWindowTitle(windowTitle)
+        self.error_dialog.showMessage(traceback.format_exc())
+        logging.error(traceback.format_exc())
+        return None
 
     def add_col(self):
         rowPosition = self.fitp1.rowCount()
@@ -546,23 +552,29 @@ class PrettyWidget(QtWidgets.QMainWindow):
         if index == 2:
             try:
                 self.loadPreset()
-            except Exception as e:
-                self.error_dialog.setWindowTitle("Error: Not able to load parametes!")
-                self.error_dialog.showMessage(traceback.format_exc())
-                logging.error(traceback.format_exc())
-                return None
+            except Exception:
+                return self.raise_error(windowTitle = "Error: Not able to load parametes!")
             self.loadPreset(x)
             # print(self.df[0], self.df[1], self.df[2])
             if len(str(self.pre[0])) != 0 and len(self.pre[1]) != 0 and len(self.pre[2]) != 0:
                 self.setPreset(self.pre[0], self.pre[1], self.pre[2])
         if index == 3:
-            self.addPreset()
+            try:
+                self.addPreset()
+            except Exception:
+                return self.raise_error("Error: could not add parameters")
             # print(self.df[0], self.df[1], self.df[2])
             if len(str(self.pre[0])) != 0 and len(self.pre[1]) != 0 and len(self.pre[2]) != 0:
                 self.setPreset(self.pre[0], self.pre[1], self.pre[2])
         if index == 4:
-            self.savePreset()
-            self.savePresetDia()
+            try:
+                self.savePreset()
+            except Exception:
+                return self.raise_error("Error: could not save parameters")
+            try:
+                self.savePresetDia()
+            except Exception:
+                return self.raise_error("Error: could not save data")
         if index == 5:
             # load C1s peak preset
             pre_bg = [[2, 295, 2, 275, '', '', '', '', '', ''], ['cv', 1e-06, 'it', 10, '', '', '', '', '', ''],
@@ -818,9 +830,18 @@ class PrettyWidget(QtWidgets.QMainWindow):
             file.close()
 
     def export_all(self):
-        self.exportResults()
-        self.savePreset()
-        self.savePresetDia()
+        try:
+            self.exportResults()
+        except Exception:
+            self.raise_error("Error: could not export the results.")
+        try:
+            self.savePreset()
+        except Exception:
+            self.raise_error("Error: could not save parameters.")
+        try:
+            self.savePresetDia()
+        except Exception:
+            self.raise_error("Error: could not save parameters / export data.")
 
     def exportResults(self):
         if not self.result.empty:
@@ -926,7 +947,10 @@ class PrettyWidget(QtWidgets.QMainWindow):
                                                                  'VMS Files (*.vms *.npl)')
             if cfilePath != "":
                 # print (cfilePath)
-                self.list_vamas = vpy.list_vms(cfilePath)
+                try:
+                    self.list_vamas = vpy.list_vms(cfilePath)
+                except Exception:
+                    return self.raise_error("Error: could not load VAMAS file.")
                 self.list_file.extend(self.list_vamas)
 
                 # print (self.list_file)
@@ -1039,9 +1063,14 @@ class PrettyWidget(QtWidgets.QMainWindow):
                                    max_rows=1)
                 # self.df = pd.read_csv(str(self.comboBox_file.currentText()), dtype = float,  skiprows=1,
                 # header=None, delimiter = '\t')
-
-            x0 = self.df[:, 0]
-            y0 = self.df[:, 1]
+            try:
+                x0 = self.df[:, 0]
+            except Exception:
+                return self.raise_error("Error: could not load csv file.")
+            try:
+                y0 = self.df[:, 1]
+            except Exception:
+                return self.raise_error("Error: could not load csv file.")
             # print(strpe)
             strpe = (str(strpe).split())
             # print(pe)
