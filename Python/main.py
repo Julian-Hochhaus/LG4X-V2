34c94d67ff6968d2ff56946389499b2dc7e2e6db
Julian Hochhaus <julian.hochhaus@tu-dortmund.de>
2023-08-05 13:58:51 +0200

    bump version 1.3.1 -> 1.3.2

--- Python/main.py
+++ Python/main.py
@@ -26,7 +26,7 @@ import threading
 
 import traceback  # error handling
 import logging  # error handling
-__version__ = "1.3.1"
+__version__ = "1.3.2"
 # style.use('ggplot')
 style.use('seaborn-v0_8-colorblind')
 dictBG = {
